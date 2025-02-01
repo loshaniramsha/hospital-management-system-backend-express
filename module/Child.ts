367a4ad9b25fc928bd8ed49475b76441327790f1
loshaniramsha <loshanikottagoda@gmail.com>
2025-02-01 10:40:08 +0530

    Added ChildEntity class for managing child records

---
+++ module/Child.ts
@@ -0,0 +1,30 @@
+export default class ChildEntity {
+    child_id: number;
+    child_name: string;
+    mother_name: string;
+    contact: string;
+    address: string;
+    age: number;
+    vaccine_status: string;
+    doctor_id: number;
+
+    constructor(
+        child_id: number,
+        child_name: string,
+        mother_name: string,
+        contact: string,
+        address: string,
+        age: number,
+        vaccine_status: string,
+        doctor_id: number
+    ) {
+        this.child_id = child_id;
+        this.child_name = child_name;
+        this.mother_name = mother_name;
+        this.contact = contact;
+        this.address = address;
+        this.age = age;
+        this.vaccine_status = vaccine_status;
+        this.doctor_id = doctor_id;
+    }
+}
