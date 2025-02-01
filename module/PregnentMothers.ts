18d66571b1781905efae944195735fda55d002d4
loshaniramsha <loshanikottagoda@gmail.com>
2025-02-01 10:19:14 +0530

    Added PregnentMothersEntity class for managing pregnant mother details

---
+++ module/PregnentMothers.ts
@@ -0,0 +1,36 @@
+export default class PregnentMothersEntity {
+    mother_id: number;
+    mother_name: string;
+    mother_age: number;
+    mother_address: string;
+    contact: string;
+    gravidity:number
+    register_date:Date
+    delivery_date:Date
+    clinic_date:Date
+    doctor_id:number
+
+    constructor(
+        mother_id: number,
+        mother_name: string,
+        mother_age: number,
+        mother_address: string,
+        contact: string,
+        gravidity:number,
+        register_date:Date,
+        delivery_date:Date,
+        clinic_date:Date,
+        doctor_id:number
+    ){
+        this.mother_id = mother_id;
+        this.mother_name = mother_name;
+        this.mother_age = mother_age;
+        this.mother_address = mother_address;
+        this.contact = contact;
+        this.gravidity = gravidity;
+        this.register_date = register_date;
+        this.delivery_date = delivery_date;
+        this.clinic_date = clinic_date;
+        this.doctor_id = doctor_id;
+    }
+}
