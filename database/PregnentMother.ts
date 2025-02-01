9231f2afdd9b56044dc670ecdc2f04fc2a0e21ec
loshaniramsha <loshanikottagoda@gmail.com>
2025-02-01 10:20:18 +0530

    Added PregnantMotherAdd function for inserting pregnant mother records using Prisma

---
+++ database/PregnentMother.ts
@@ -0,0 +1,26 @@
+import { PrismaClient } from "@prisma/client";
+import Mothers from "../module/PregnentMothers"
+
+const prisma=new PrismaClient();
+export async function PregnantMotherAdd(mother:Mothers){
+    try {
+        const newMother=await prisma.pregnantMother.create({
+            data:{
+                mother_name:mother.mother_name,
+                mother_age:mother.mother_age,
+                mother_address:mother.mother_address,
+                contact:mother.contact,
+                gravidity:mother.gravidity,
+                register_date:mother.register_date,
+                delivery_date:mother.delivery_date,
+                clinic_date:mother.clinic_date,
+                doctor_id:mother.doctor_id
+            }
+        })
+        console.log("Mother Added Successfully",newMother)
+    }
+    catch(err){
+        console.log("error fetching data",err)
+        throw err;
+    }
+}
