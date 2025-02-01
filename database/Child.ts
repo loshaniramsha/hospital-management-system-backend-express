31849040ae28942a5e8e1c1debc074499934cfa2
loshaniramsha <loshanikottagoda@gmail.com>
2025-02-01 10:40:51 +0530

    Implemented ChildAdd function to insert new child records using Prisma

---
+++ database/Child.ts
@@ -0,0 +1,26 @@
+import { PrismaClient } from "@prisma/client";
+import Child from "../module/Child";
+
+const prisma=new PrismaClient();
+
+export async function ChildAdd(child:Child){
+    try {
+        const newChild=await prisma.children.create({
+            data:{
+                child_name:child.child_name,
+                mother_name:child.mother_name,
+                contact:child.contact,
+                address:child.address,
+                age:child.age,
+                vaccine_status:child.vaccine_status,
+                doctor_id:child.doctor_id
+            }
+        })
+        console.log("Child Added Successfully",newChild)
+
+    }
+    catch(err){
+        console.log("error fetching data",err)
+        throw err;
+    }
+}
