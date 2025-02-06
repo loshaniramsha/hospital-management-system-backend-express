748fa73a2cc2d78fa7ee95ec198e7af2989d8f4d
loshaniramsha <loshanikottagoda@gmail.com>
2025-02-06 06:05:51 +0530

    Added DELETE endpoint for child removal with error handling

--- route/ChildRoute.ts
+++ route/ChildRoute.ts
@@ -1,6 +1,6 @@
 import express from "express";
 import Child from "../module/Child"
-import {ChildAdd} from "../database/Child";
+import {ChildAdd,DeleteChild} from "../database/Child";
 
 const router=express.Router();
 router.post("/add",async (req,res)=>{
@@ -12,6 +12,17 @@ router.post("/add",async (req,res)=>{
     catch (err){
         console.log(err);
         res.status(400).send({err:err})
+    }
+})
+/*Delete Child*/
+router.delete("/delete/:id",async (req,res)=>{
+    const id:number=+req.params.id;
+    try {
+        await DeleteChild(id);
+        res.send("Child Deleted");
+    }
+    catch (e){
+        console.log("Not Success Child Delete",e);
     }
 })
 export default router;
